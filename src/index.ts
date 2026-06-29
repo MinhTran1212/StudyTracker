d619bce06c22b2fbdf25022a800817163d46f4bd
MinhTran1212 <tranluongnhatminh2007@gmail.com>
2026-06-29 16:54:16 +0700

    small adjustment

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,9 @@ app.use(express.json());
 const PORT = process.env.PORT || 3000;
 
 connectDB();
-
+app.get('/', (req, res) => {
+    res.status(200).send("🚀 StudyTracker API is officially live and kicking in the cloud!");
+});
 app.use('/subjects', subjectRoutes);
 app.use('/studysessions', studySessionRoutes);
 app.use('/register', authRoutes);
